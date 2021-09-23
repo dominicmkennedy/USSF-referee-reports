d508715aa7036eee50b0b2afec805c33f8ada482
dominicmkennedy <dominicmkennedy@gmail.com>
2021-09-23 17:02:46 -0400

    refoctored web server to allow for html templating

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
         "fmt"
         "log"
         "net/http"
+        "html/template"
 
         "github.com/gorilla/schema"
         "github.com/leebenson/conform"
@@ -29,15 +30,28 @@ func PostForm(w http.ResponseWriter, r *http.Request) {
 
         addtoDB(form)
         writePDF(form)
+        StorePDF(form)
+
+        ip := r.Header.Get("X-FORWARDED-FOR")
+        fmt.Fprintf(w, "IP: %T\n", ip)
+        fmt.Fprintf(w, "IP: %s\n", ip)
 
         fmt.Fprintf(w, "report: %T\n", form)
         fmt.Fprintf(w, "report: %s\n", form)
 
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+        //http.ServeFile(w, r, "./static/")
+        tmpl := template.Must(template.ParseFiles("./static/index.html")) 
+        tmpl.Execute(w, nil)
+}
+
 func main() {
 
-        http.Handle("/", http.FileServer(http.Dir("./static")))    
+        //http.Handle("/", http.FileServer(http.Dir("./static")))    
+        http.Handle("/script.js", http.FileServer(http.Dir("./static")))    
+        http.HandleFunc("/", index)
         http.HandleFunc("/submit/", PostForm)
 
         log.Fatal(http.ListenAndServe(":8080", nil))
