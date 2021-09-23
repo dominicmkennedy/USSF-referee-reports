29e7cc94bc93e87958f09697fd2da02c5a9909bf
dominicmkennedy <dominicmkennedy@gmail.com>
2021-09-22 22:40:58 -0400

    exported my testing js functions

---
+++ static/script.js
@@ -0,0 +1,20 @@
+function addCaution() {
+        var temp = document.getElementById("caution").content;
+        var clon = document.importNode(temp, true);
+        document.getElementById("cautions").appendChild(clon);
+}
+function addRed() {
+        var temp = document.getElementById("red").content;
+        var clon = document.importNode(temp, true);
+        document.getElementById("reds").appendChild(clon);
+}
+function addSupplemental() {
+        var temp = document.getElementById("supplemental").content;
+        var clon = document.importNode(temp, true);
+        document.getElementById("supplementals").appendChild(clon);
+}
+function addEmail() {
+        var temp = document.getElementById("email").content;
+        var clon = document.importNode(temp, true);
+        document.getElementById("emails").appendChild(clon);
+}
