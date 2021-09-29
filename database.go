80a32e243253ad955e4514e2fb7614dc2a16bbf7
dominicmkennedy <dominicmkennedy@gmail.com>
2021-09-29 00:10:37 -0400

    modifying the DB to store and update lists of players and their sanctions

--- database.go
+++ database.go
@@ -6,6 +6,9 @@ import (
 
         firebase "firebase.google.com/go"
         "google.golang.org/api/option"
+        "github.com/gogo/status"
+        "google.golang.org/grpc/codes"
+        "cloud.google.com/go/firestore"
 )
 
 func addtoDB(form *refereeReport) {
@@ -45,17 +48,17 @@ func addtoDB(form *refereeReport) {
                 "FourthOfficialName":           form.FourthOfficialName,
                 "FourthOfficialGrade":          form.FourthOfficialGrade,
 
-                                
+
                 "CautionPlayerName":            form.CautionPlayerName,
                 "CautionPlayerID":              form.CautionPlayerID,
                 "CautionTeam":                  form.CautionTeam,
                 "CautionCode":                  form.CautionCode,
-                
+
                 "RedPlayerName":                form.RedPlayerName,
                 "RedPlayerID":                  form.RedPlayerID,
                 "RedTeam":                      form.RedTeam,
                 "RedCode":                      form.RedCode,
-                
+
                 "SupplementalStatement":        form.SupplementalStatement,
                 "SupplementalLocationX":        form.SupplementalLocationX,
                 "SupplementalLocationY":        form.SupplementalLocationY,
@@ -66,7 +69,7 @@ func addtoDB(form *refereeReport) {
                 "USSFID":                       form.USSFID,
                 "ContactNumber":                form.ContactNumber,
                 "ContactEmail":                 form.ContactEmail,    
-                
+
                 "ipaddr":                       form.ipaddr,
                 "SubmittedTime":                form.SubmittedTime,
                 "ReportID":                     form.ReportID,
@@ -77,5 +80,34 @@ func addtoDB(form *refereeReport) {
         }
 
         form.ReportID = DocRef.ID
+
+        PlayerID        := form.CautionPlayerID[0]
+        PlayerName      := form.CautionPlayerName[0]
+        PlayerCautions  := make([]interface {}, 0)
+
+        
+        dsnap, err := client.Collection("players").Doc(PlayerID).Get(ctx)
+        if status.Code(err) == codes.NotFound {
+                log.Printf("doc does not yet exist creating doc\n")
+                _, err = client.Collection("players").Doc(PlayerID).Set(ctx, map[string]interface{}{
+                        "Name":     PlayerName,
+                        "Cautions": PlayerCautions,
+                })
+                dsnap, err = client.Collection("players").Doc(PlayerID).Get(ctx)
+
+        }  
+        if err != nil {
+                log.Fatal(err)
+        } 
+
+        m := dsnap.Data()
+        log.Printf("Player data: %#v\n", m)
+        
+        PlayerCautions = append(PlayerCautions, m["Cautions"].([]interface {})...) 
+        PlayerCautions = append(PlayerCautions, form.CautionCode[0]) 
+       
+       _, err = client.Collection("players").Doc(PlayerID).Set(ctx, map[string]interface{}{
+                "Cautions":         PlayerCautions, 
+        }, firestore.MergeAll)
 
 }
