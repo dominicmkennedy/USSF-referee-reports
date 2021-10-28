4d3b4d65f661c5e3e0ac850c88a13a269595ade9
dominicmkennedy <dominicmkennedy@gmail.com>
2021-10-28 00:17:50 -0400

    moved firestore.go to storage.go

---
+++ src/storage.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "context"
+    "io"
+    "os"
+    "time"
+    "log"
+
+    "cloud.google.com/go/storage"
+    "google.golang.org/api/option"
+)
+
+func uploadFile(bucket, object string) error {
+
+    ctx := context.Background()
+    opt := option.WithCredentialsFile("../creds.json")
+
+    client, err := storage.NewClient(context.Background(), opt)
+    if err != nil {
+        return err
+    }
+    defer client.Close()
+
+    // Open local file.
+    f, err := os.Open("../reports/" + object + ".pdf")
+    if err != nil {
+        return err
+    }
+    defer f.Close()
+
+    ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+    defer cancel()
+
+    wc := client.Bucket(bucket).Object(object + ".pdf").NewWriter(ctx)
+    if _, err = io.Copy(wc, f); err != nil {
+        return err
+    }
+
+    if err := wc.Close(); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func (PDF *PDFReport) StorePDF() {
+    err := uploadFile ( "tnsoccerreports-testing.appspot.com", PDF.ReportID)
+    if err != nil {
+        log.Println(err)
+    }
+}
