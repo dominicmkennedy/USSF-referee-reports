9b847f59f7f1f9b0c1a6ec3b0603e89859dfb55d
dominicmkennedy <dominicmkennedy@gmail.com>
2021-12-09 01:01:51 -0500

    restarted report sanitization, things are currently a touch messy

--- src/report.go
+++ src/report.go
@@ -4,6 +4,7 @@ import (
     "time"
     "reflect"
     "strconv"
+    "net/mail"
 )
 
 type Sanction struct {
@@ -63,23 +64,146 @@ type POSTReport struct {
 
 }
 
-//TODO the rest of this function
 func (r *POSTReport) SanitizePostData() {
 
+    // set the reportID to whatever firestore generates
     r.ReportID = GetReportID()
 
-    if  len(r.Cautions) > 30 {
-        r.Cautions = r.Cautions[:30]
+    // trim slice lengths to reasonable amounts in the case of POST request tampering
+    if len(r.Cautions) > 30 { r.Cautions = r.Cautions[:30] }
+    if len(r.SendOffs) > 15 { r.SendOffs = r.SendOffs[:15] }
+    if len(r.Supplementals) > 5 { r.Supplementals = r.Supplementals[:5] }
+    if len(r.SendToEmail) > 30 { r.SendToEmail = r.SendToEmail[:30] }
+
+    /*
+    // name shouldn't have an issue except maybe length
+    HomeTeamName            string
+    //score should convert to an int w/o error
+    HomeTeamScore           string
+    AwayTeamName            string
+    AwayTeamScore           string
+
+    // conform w/ map
+    // if else blocks would be quicker than a map
+    PlayerSex               string
+    // honestly maybe regex idrk
+    PlayerAge               string
+
+    GameAssociation         string
+    GameDivision            string
+    GameLeague              string
+    GameNumber              string
+    GameDate                time.Time
+    SubmittedDate           time.Time
+
+    RefereeName             string
+    RefereeGrade            string
+    AssistantReferee1Name   string
+    AssistantReferee1Grade  string
+    AssistantReferee2Name   string
+    AssistantReferee2Grade  string
+    FourthOfficialName      string
+    FourthOfficialGrade     string
+
+    Cautions                []Sanction
+    SendOffs                []Sanction
+    PlayerRole       string
+    PlayerName       string
+    PlayerID         string
+    Team             string
+    Code             string
+
+    Supplementals           []Supplemental
+    Statement   string
+    LocationX   string
+    LocationY   string
+
+    ReporterName            string
+    ReporterUSSFID          string
+    ReporterPhone           string
+
+    */
+
+    SanitizeEmail(&r.ReporterEmail)
+
+    for i := range r.SendToEmail {
+        SanitizeEmail(&r.SendToEmail[i])
     }
 
-    if  len(r.SendOffs) > 15 {
-        r.SendOffs = r.SendOffs[:15]
+
+    // TODO scope this as both global and constant
+    // so it is declared once at runtime
+    // and is imutable
+    States := map[string]struct{} {
+        "AL": {},
+        "AK": {},
+        "AZ": {},
+        "AR": {},
+        "CA": {},
+        "CO": {},
+        "CT": {},
+        "DE": {},
+        "DC": {},
+        "FL": {},
+        "GA": {},
+        "HI": {},
+        "ID": {},
+        "IL": {},
+        "IN": {},
+        "IA": {},
+        "KS": {},
+        "KY": {},
+        "LA": {},
+        "ME": {},
+        "MD": {},
+        "MA": {},
+        "MI": {},
+        "MN": {},
+        "MS": {},
+        "MO": {},
+        "MT": {},
+        "NE": {},
+        "NV": {},
+        "NH": {},
+        "NJ": {},
+        "NM": {},
+        "NY": {},
+        "NC": {},
+        "ND": {},
+        "OH": {},
+        "OK": {},
+        "OR": {},
+        "PA": {},
+        "RI": {},
+        "SC": {},
+        "SD": {},
+        "TN": {},
+        "TX": {},
+        "UT": {},
+        "VT": {},
+        "VA": {},
+        "WA": {},
+        "WV": {},
+        "WI": {},
+        "WY": {},
     }
 
-    if  len(r.Supplementals) > 5 {
-        r.Supplementals = r.Supplementals[:5]
-    }
+    IsStringInMap(&r.AwayTeamState, &States)
+    IsStringInMap(&r.HomeTeamState, &States)
 
+}
+
+func IsStringInMap(str * string, m * map[string]struct{}) {
+    if _, found := (*m)[*str]; !found {
+        *str = ""
+    }
+}
+
+func SanitizeEmail(Email * string) {
+    _, err := mail.ParseAddress(*Email)
+    if err != nil {
+        *Email = ""
+    }
 }
 
 func DateConverter(POSTString string) (reflect.Value) {
