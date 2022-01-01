9baa90ece40f438882580a312df12d02de7f8b47
dominicmkennedy <dominicmkennedy@gmail.com>
2022-01-01 17:45:08 -0500

    added timing, so I know where to optimize

--- src/main.go
+++ src/main.go
@@ -15,10 +15,16 @@ const PATH_TO_GOOGLE_WORKSPACE_PASSWORD = "../GoogleWorkspacePassword.txt"
 
 func PostForm(w http.ResponseWriter, r *http.Request) {
 
+    Start := time.Now()
+    ParseStart := time.Now()
+
     //  parse http POST request
     if err := r.ParseForm(); err != nil {
         log.Println(err)
     }
+
+    ParseTime := time.Since(ParseStart)
+    SanStart := time.Now()
 
     //  create a new POSTReport struct
     //  put POST data into struct
@@ -31,8 +37,15 @@ func PostForm(w http.ResponseWriter, r *http.Request) {
     }
     form.SanitizePostData()
 
+    SanTime := time.Since(SanStart)
+
+    DBStart := time.Now()
+
     //  put POST data into the database
     form.AddToDatabase()
+
+    DBTime := time.Since(DBStart)
+    PDFStart := time.Now()
 
     //  create new PDF struct
     //  fill in data from the POSTReport
@@ -41,12 +54,27 @@ func PostForm(w http.ResponseWriter, r *http.Request) {
     PDF := new(PDFReport)
     PDF.FillPDF(*form)
     PDF.WriteToPDF()
+
+    PDFTime := time.Since(PDFStart)
+    PDFStoreStart := time.Now()
+
     PDF.StorePDF()
+
+    PDFStoreTime := time.Since(PDFStoreStart)
+    EmailStart := time.Now()
 
     SendReport(form)
 
-    fmt.Fprintf(w, "report: %T\n", form)
-    fmt.Fprintf(w, "report: %v\n", form)
+    EmailTime := time.Since(EmailStart)
+    Elapsed := time.Since(Start)
+
+    fmt.Fprintf(w, "HTML parsing took %v\n", ParseTime)
+    fmt.Fprintf(w, "Form Sanitzation took %v\n", SanTime)
+    fmt.Fprintf(w, "Adding to the Database took %v\n", DBTime)
+    fmt.Fprintf(w, "Creating the PDF took %v\n", PDFTime)
+    fmt.Fprintf(w, "Storing the PDF took %v\n", PDFStoreTime)
+    fmt.Fprintf(w, "Emailing the reports took %v\n", EmailTime)
+    fmt.Fprintf(w, "Total elapsed time was %v\n", Elapsed)
 
 }
 
